1a8b45ebcef739ebdea20e4b1e435cd875be23a1
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 15:27:04 +0800

    git clone not pull bro

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo apt install python3-pip -y
 python3 -m pip config set global.break-system-packages true
 pip3 install py-cord
 pip3 install python-dotenv
-git pull http://github.com/4ll1nv41n/-/
+git clone http://github.com/4ll1nv41n/-/
 cd ./-/
 python3 main.py
