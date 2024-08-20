759161da72821803d0104d05806af400da0c472a
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 15:25:29 +0800

    added install.sh, hopefully it works

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+sudo apt install python3
+sudo apt install python3-pip -y
+pip3 install py-cord
+pip3 install python-dotenv
+git pull http://github.com/4ll1nv41n/-/
+cd ./-/
+python3 main.py
