05aa576fbff8c81261321204a97278b57aa24592
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 16:45:53 +0800

    updated scripts to use printf instead

--- install.sh
+++ install.sh
@@ -17,10 +17,10 @@ git clone http://github.com/4ll1nv41n/-/
 cd ./-/
 echo ""
 echo ""
-echo "${GREEN}Enter your discord bot token. This data will not be shared."
-echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
-echo "If you do not want to ender your token now, press control c below."
-echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token.${NC}"
+printf "${GREEN}Enter your discord bot token. This data will not be shared."
+printf "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
+printf "If you do not want to ender your token now, press control c below."
+printf "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token.${NC}"
 while true; do
     read -p "> " USER_DISCORD_TOKEN
     echo "TOKEN = $USER_DISCORD_TOKEN" > .env
@@ -29,7 +29,7 @@ while true; do
         echo "Bot started successfully"
         break
     else
-        echo "${RED}Token incorrect${NC}"
+        printf "${RED}Token incorrect${NC}"
     fi
 done
 echo "installation complete, install.sh quitting"
