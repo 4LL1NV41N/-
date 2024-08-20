58b528c107105b064362cc2cc4e44849352b3b66
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 15:15:21 +0800

    testing

--- install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install git
-sudo apt install python3
-sudo apt install python3-pip -y
-git clone https://github.com/4LL1NV41N/-.git
-cd ./-/
-python3 -m pip config set global.break-system-packages true
-echo "bot is installed, use python3 main.py to run bot"
