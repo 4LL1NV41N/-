876799d1bb8b3231b3afd13f353a3e39ec42cd7e
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 16:23:58 +0800

    fixed script and added colors!!!

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 sudo apt update
 sudo apt upgrade
 sudo apt install git
@@ -11,23 +15,20 @@ pip3 install py-cord
 pip3 install python-dotenv
 git clone http://github.com/4ll1nv41n/-/
 cd ./-/
-echo "Enter your discord bot token. This data will not be shared."
+echo ""
+echo ""
+echo "${GREEN}Enter your discord bot token. This data will not be shared."
 echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
-echo "If you do not want to ender your token now, type 'no thanks' below."
-echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token."
+echo "If you do not want to ender your token now, press control c below."
+echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token.${NC}"
 while true; do
     read -p "> " USER_DISCORD_TOKEN
-
-    if [[ "$USER_DISCORD_TOKEN" == "no thanks" ]]; then
-        echo "please add your token to ./.env before running the bot"
-    fi 
-
     echo "TOKEN = $USER_DISCORD_TOKEN" > .env
 
     if python3 main.py; then
-        echo "bot started successfully"
+        echo "Bot started successfully"
         break
     else
-        echo "Token incorrect"
+        echo "${RED}Token incorrect${NC}"
     fi
 done
