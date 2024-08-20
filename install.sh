82df88d9b379c1f5ad19f408aecef61a8b4e958e
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-08-20 15:57:28 +0800

    attempt to validate input lol

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 sudo apt update
 sudo apt upgrade
 sudo apt install git
@@ -8,4 +11,23 @@ pip3 install py-cord
 pip3 install python-dotenv
 git clone http://github.com/4ll1nv41n/-/
 cd ./-/
-python3 main.py
+echo "Enter your discord bot token. This data will not be shared."
+echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
+echo "If you do not want to ender your token now, type 'no thanks' below."
+echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token."
+while true; do
+    read -p "> " USER_DISCORD_TOKEN
+
+    if [[ "$USER_DISCORD_TOKEN" == "no thanks" ]]; then
+        echo "please add your token to ./.env before running the bot"
+    fi 
+
+    echo "TOKEN = $USER_DISCORD_TOKEN" > .env
+
+    if python3 main.py; then
+        echo "bot started successfully"
+        break
+    else
+        echo "Token incorrect"
+    fi
+done
