c2fc7af8bc1657ab93486f40a3ca40eb70831751
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-12 23:20:44 +0800

    why didnt i think of this before

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ load_dotenv()
 print("loaded dotenv")
 print("bot starting... please hold on for a moment...")
 
-SECRET = "maestrefi"                                                                    # this is the secret phrase
-RESPONSE = "insert teh end of the youtube link here!!! :33"                             # this is the clue that the bot will drop
-YAP = f"Congrats blah blah balh here is the next clue ```{RESPONSE}``` blah blah"       # this is whatever the bot yaps about. you can use '''triple quotes''' if you want
-__TOKEN = str(os.getenv("TOKEN"))                                                       # put your token in .env https://guide.pycord.dev/getting-started/creating-your-first-bot#using-dotenv 
-# DO NOT PUBLISH YOUR DISCORD TOKEN ONLINE. IT SHOULD NOT BE SHARED
+SECRET = "maestrefi"                                                                        # this is the secret phrase
+RESPONSE = "insert teh end of the youtube link here!!! :33"                                 # this is the clue that the bot will drop
+YAP = "Congrats blah blah balh here is the next clue ```" + RESPONSE + "``` blah blah"      # optimizing concatenation
+__TOKEN = os.getenv("TOKEN")                                                                # use getenv without casting to string, it's already a string in env vars
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()  # use only required intents to save memory
 
 # init bot
 try:
@@ -23,80 +22,30 @@ try:
 except Exception as e:
     print(f"some error occured while trying to load the bot :((\nSTART OF OUTPUT\n{e}\nEND OF OUTPUT\naw man :(")
 
-async def send_random_emoji():
-    await client.wait_until_ready()
-    channel = client.get_channel(1273798734253133827) 
-    while not client.is_closed():
-        await asyncio.sleep(random.randint(2916, 8748))
-        if random.randint(0,1) == 1:
-            print("sent <:dimini:1273803816357199873> lol")
-            await channel.send('<:dimini:1273803816357199873>')
-            await channel.send(file=discord.File('/Users/nat/Downloads/DIMINI.mp4'))
-        else:
-            file = discord.File("https://ibb.co/kmrcmn8", filename="image.png")
-            embed = discord.Embed()
-            embed.set_image(url="attachment://image.png")
-            await channel.send(file=file, embed=embed)
-        
-        
 @client.command(name="dimini")
 async def dimini(ctx):
     print("sent <:dimini:1273803816357199873> lol")
-    await ctx.response('<:dimini:1273803816357199873>')
-    await ctx.response(file=discord.File('/Users/nat/Downloads/DIMINI.mp4'))
-    
-@client.command(name="george")
-async def george(ctx):
-    print("george")
-    file = discord.File("https://ibb.co/kmrcmn8", filename="image.png")
-    embed = discord.Embed()
-    embed.set_image(url="attachment://image.png")
-    await ctx.response(file=file, embed=embed)
+    await ctx.respond('<:dimini:1273803816357199873>')
+    await ctx.respond(file=discord.File('DIMINI.mp4'))
 
 @client.command(name='say')
 async def say(ctx, *, message: str):
-    print(f"say used: {message}")
-    try:
-        await ctx.message.delete()
-    except Exception as e:
-        print(f"error while using ^say: {e}")
     await ctx.respond(message)
-    
-async def saycmd(ctx, channel):
-    message = ctx.content[5::]
-    print(f"say used: {message}")
-    try:
-        await ctx.message.delete()
-    except Exception as e:
-        print(f"error while using ^say: {e}")
-    await channel.send(message)
+
 @client.event
 async def on_ready():
     print(f"logged in as {client.user}")
     print("bot is running!! have fun!! :3")
     print("made with love by ellipticobj :3c")
-    client.loop.create_task(send_random_emoji())
 
 @client.event
 async def on_message(ctx):
     print(f"message \"{ctx.content}\" sent by user {ctx.author}")
-    s = ctx.content
-    print(s)
-    if ctx.content[0:4] == "^say":
-        channel = client.get_channel(ctx.channel)
-        message = ctx.content[5::]
-        print(f"say used: {message}")
+    if ctx.content.lower().strip() == SECRET and ctx.author.id != client.user.id:
         try:
             await ctx.message.delete()
         except Exception as e:
-            print(f"error while using ^say: {e}")
-        await channel.send(message)
-    elif ctx.content.lower().strip() == SECRET and ctx.author.id != client.user.id:
-        try:
-            await ctx.message.delete()
-        except Exception as e:
-            print(f"some error occured while trying to load the bot :((\nSTART OF OUTPUT\n{e}\nEND OF OUTPUT\naw man :(")
-
+            print(f"some error occured while trying to delete the message :((\nSTART OF OUTPUT\n{e}\nEND OF OUTPUT\naw man :(")
         print(f"woah {ctx.author} got it right!!")
         await ctx.author.send(YAP)
         print(f"clue given!! :3")
@@ -104,6 +53,7 @@ async def on_message(ctx):
         print(f"tehy got it wrong")
 
 client.run(__TOKEN)
+
 # (c) Copyright 2024 Natalie http://github.com/ellipticobj http://ithub.com/mysteriousellipsis
 # (c) Copyright 2024 Thern http://github.com/DystopianDood09 http://github.com/4LL1NV4IN
 
