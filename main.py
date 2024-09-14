7e8bd790e8d25ead235c4c34deccb51110f03333
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-14 23:24:07 +0800

    dimini

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def on_message(message):
         data[message.author.id] += 1
     else:
         data[message.author.id] = 1
-    if data[message.author.id] < 20:
+    if data[message.author.id] < 20 and ratelimiting:
         print(f"message \"{message.content}\" sent by user {message.author}")
         if message.content.lower().strip() == SECRET and message.author.id != client.user.id:
             try:
@@ -60,46 +60,50 @@ async def on_message(message):
             print(f"clue given!! :3")
         else:
             print(f"tehy got it wrong")
-    else:
+    elif data[message.author.id] >= 20 and ratelimiting:
         message.response("You are sending requests too quickly.", ephemeral=True)
-        
+
 async def clear_rate():
     while True:
         if clearingrates:
             print("clearing rate")
             try:
-                with open("rate.json","wr") as file:
+                with open("rate.json","r") as file:
                     data = json.load(file)
-                    for key in data:
-                        data[key] = 0
-                    json.dump(data, file)
+                for key in data:
+                    data[key] = 0
+                with open("rate.json", "w") as file:
+                    json.dump(data, file, indent=4)
+                    print("rates cleared")
             except Exception as e:
                 print(f"an error occured while trying to clear rates: {e}")
+                print("rates not cleared, rate limiting turned off")
+                ratelimiting = False
         else:
             print("clearing rates turned off")
         await asyncio.sleep(3600)
-        
+
 
 client.run(__TOKEN)
 
 # (c) Copyright 2024 Natalie http://github.com/ellipticobj http://ithub.com/mysteriousellipsis
 # (c) Copyright 2024 Thern http://github.com/DystopianDood09 http://github.com/4LL1NV4IN
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of 
-# this software and associated documentation files (the “Software”), to deal in the 
-# Software without restriction, including without limitation the rights to use, copy, 
-# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-# and to permit persons to whom the Software is furnished to do so, subject to the 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the “Software”), to deal in the
+# Software without restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the
 # following conditions:
 
-# The above copyright notice and this permission notice shall be included in all copies 
+# The above copyright notice and this permission notice shall be included in all copies
 # or substantial portions of the Software.
 
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# https://opensource.org/license/mit    
+# https://opensource.org/license/mit
