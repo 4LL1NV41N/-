3e9d672ac167885198e52f1cac4dcfc810573d38
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-15 00:10:07 +0800

    working yay

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ intents = discord.Intents.all()  # use only required intents to save memory
 
 # init bot
 try:
-    client = discord.Bot(intents=intents)
+    client = discord.Bot(intents=intents,debug_guilds=[1273798733703675976])
 except Exception as e:
     print(f"some error occured while trying to load the bot :((\nSTART OF OUTPUT\n{e}\nEND OF OUTPUT\naw man :(")
 
@@ -33,6 +33,48 @@ async def dimini(ctx):
 async def say(ctx, *, message: str):
     await ctx.respond(message)
 
+ratelimit = discord.SlashCommandGroup("ratelimit", "enable or disable rate limiting")
+
+@ratelimit.command(name="enable")
+async def ratelimitenable(ctx):
+    global ratelimiting
+    ratelimiting = True
+    await ctx.respond(f"rate limiting toggled to {ratelimiting}", ephemeral=True)
+
+@ratelimit.command(name="disable")
+async def ratelimitdisable(ctx):
+    global ratelimiting
+    ratelimiting = False
+    await ctx.respond(f"rate limiting toggled to {ratelimiting}", ephemeral=True)
+
+clearrates = discord.SlashCommandGroup("clearrates", "enable or disable rate clearing")
+
+@clearrates.command()
+async def clear(ctx):
+    try:
+        with open("rate.json","r") as file:
+            data = json.load(file)
+        for key in data:
+            data[key] = 0
+        with open("rate.json", "w") as file:
+            json.dump(data, file, indent=4)
+            await ctx.respond("rates cleared", ephemeral=True)
+    except Exception as e:
+        await ctx.respond(f"an error occured while trying to clear rates: {e}", ephemeral=True)
+        await ctx.respond("rates not cleared", ephemeral=True)
+
+@clearrates.command(name="enable")
+async def enableclearrates(ctx):
+    global clearrates
+    clearrates = True
+    await ctx.respond(f"rate clearing toggled to {clearrates}", ephemeral=True)
+
+@clearrates.command(name="disable")
+async def disableclearrates(ctx):
+    global clearrates
+    clearrates = False
+    await ctx.respond(f"rate clearing toggled to {clearrates}", ephemeral=True)
+
 @client.event
 async def on_ready():
     print(f"logged in as {client.user}")
@@ -42,15 +84,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    file = open("rate.json", "wr")
-    data = json.load(file)
-    if message.author.id in data:
-        data[message.author.id] += 1
-    else:
-        data[message.author.id] = 1
-    if data[message.author.id] < 20 and ratelimiting:
+    if message.author == client.user:
+        return
+
+    # handling rate limiting n stuff
+    with open("rate.json", "r") as file:
+        data = json.load(file)
+
+    user_id = str(message.author.id)
+    data[user_id] = data.get(user_id, 0) + 1
+
+    with open("rate.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    if data[str(message.author.id)] < 20 and ratelimiting:
         print(f"message \"{message.content}\" sent by user {message.author}")
-        if message.content.lower().strip() == SECRET and message.author.id != client.user.id:
+        if message.content.lower().strip() == SECRET:
             try:
                 await message.delete()
             except Exception as e:
@@ -60,8 +109,10 @@ async def on_message(message):
             print(f"clue given!! :3")
         else:
             print(f"tehy got it wrong")
-    elif data[message.author.id] >= 20 and ratelimiting:
-        message.response("You are sending requests too quickly.", ephemeral=True)
+    elif data[str(message.author.id)] >= 20 and ratelimiting:
+        await message.author.send("You are sending requests too quickly.")
+    elif not ratelimiting:
+        print("rate limiting off.")
 
 async def clear_rate():
     while True:
@@ -78,12 +129,14 @@ async def clear_rate():
             except Exception as e:
                 print(f"an error occured while trying to clear rates: {e}")
                 print("rates not cleared, rate limiting turned off")
+                global ratelimiting
                 ratelimiting = False
         else:
             print("clearing rates turned off")
         await asyncio.sleep(3600)
 
-
+client.add_application_command(ratelimit)
+client.add_application_command(clearrates)
 client.run(__TOKEN)
 
 # (c) Copyright 2024 Natalie http://github.com/ellipticobj http://ithub.com/mysteriousellipsis
