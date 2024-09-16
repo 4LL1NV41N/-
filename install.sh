d942d9eae22b9930ff4faf24f8b9b50ff94e720d
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-16 13:32:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ git clone http://github.com/4ll1nv41n/-/
 cd ./-/
 echo ""
 echo ""
-echo "Enter your discord bot token. This data will not be shared."
-echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
-echo "If you do not want to ender your token now, press control c below."
-echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token."
+echo "Enter your discord bot token. This data will not be shared.\n"
+echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot\n"
+echo "If you do not want to ender your token now, press control c below.\n"
+echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token.\n"
 while true; do
     read -p "> " USER_DISCORD_TOKEN
     echo "TOKEN = $USER_DISCORD_TOKEN" > .env
@@ -26,5 +26,6 @@ while true; do
         break
     else
         echo "Token incorrect"
+        echo ""
     fi
 done
