ddf50a50269e852399223c4239a4f45e002f3c01
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-17 11:51:02 +0800

    update macos installer
    congrats to my one macos user (me)

--- install-macos.sh
+++ install-macos.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+echo "You are about to download a discord bot at ./-/"
+echo "Any files in ./-/ WILL BE DELETED PERMANENTLY"
+echo "You may be prompted when the script is installing dependancies. Please answer with Y or the bot may not work"
+echo "Press ENTER to continue with installation"
+read
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
-
-
+rm -rf ./-/
 python3 -m ensurepip
 python3 -m pip config set global.break-system-packages true
 pip3 install py-cord
 pip3 install python-dotenv
 git clone http://github.com/4ll1nv41n/-/
 cd ./-/
+touch rate.json
 echo ""
 echo ""
-printf "${GREEN}Enter your discord bot token. This data will not be shared."
-printf "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
-printf "If you do not want to ender your token now, press control c below."
-printf "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token.${NC}"
+echo "Enter your discord bot token. This data will not be shared."
+echo "If you do not know what that is, refer to this guide: https://guide.pycord.dev/getting-started/creating-your-first-bot"
+echo "If you do not want to ender your token now, press control c below."
+echo "Your token will be stored in ./.env, in the format 'TOKEN = YOUR_TOKEN_HERE'. You can change it if you reset your token."
 while true; do
     read -p "> " USER_DISCORD_TOKEN
     echo "TOKEN = $USER_DISCORD_TOKEN" > .env
@@ -26,7 +28,6 @@ while true; do
         echo "Bot started successfully"
         break
     else
-        printf "${RED}Token incorrect${NC}"
+        echo "Token incorrect"
     fi
 done
-echo "installation complete, install.sh quitting"
