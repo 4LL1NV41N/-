3181d34c6b730d8b5b4c3a2f678c00bdbd8b60bf
four hundred kilovolts of electricity suddenly passes through a mysterious elliptic object floating in the sky <ellipticobj@gmail.com>
2024-09-18 20:16:42 +0800

    no cm

--- main.py
+++ main.py
@@ -63,28 +63,6 @@ if __name__ == '__main__':
         except Exception as e:
             logger.error(f"Failed to load {extension}: {e}")
 
-        
-@client.command(name="reload")
-async def reload(ctx):
-    try:
-        importlib.reload(messagehandler)
-        importlib.reload(readyhandler)
-        await ctx.send("Event handlers reloaded")
-        logger.info("reloaded main event handlers")
-    except Exception as e:
-        await ctx.send(f"Error reloading: {e}")
-        logger.error("event handlers could not be reloaded")
-        logger.error(e)
-
-@client.command(name='reloadcog')
-async def reload_cog(ctx, cog: discord.Option(str, choices=cognames)):
-    try:
-        client.reload_extension(f'cogs.{cog}')
-        await ctx.send(f"Reloaded {cog} cog.")
-    except Exception as e:
-        await ctx.send(f"Failed to reload {cog} cog: {e}")
-
-
 # discord events
 @client.event
 async def on_ready():
